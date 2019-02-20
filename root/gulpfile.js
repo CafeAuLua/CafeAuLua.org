3df4a40a74bc3da5c7bf1c9323faf44f73bd0942
James Upjohn <jspartan250@gmail.com>
2019-02-21 11:31:42 +1300

    Dockerize and add copyHTML and dist gulp task

--- root/gulpfile.js
+++ root/gulpfile.js
@@ -1,3 +1,4 @@
+const cpydir = require('copy-dir');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
@@ -7,19 +8,25 @@ const destOpts = {
     sourcemaps: "stylesheet.css.map"
 };
 
+gulp.task('copyHTML', () => {
+  cpydir.sync("./html/", "./dist");
+});
+
 gulp.task('sass', () => {
     return gulp.src('scss/stylesheet.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./', destOpts))
+        .pipe(gulp.dest('./dist', destOpts))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
+gulp.task('dist', gulp.task('sass'), gulp.task('copyHTML'));
+
 gulp.task('browserSync', () => {
   browserSync.init({
     server: {
-      baseDir: './'
+      baseDir: './dist'
     },
   })
 });
